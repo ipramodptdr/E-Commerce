6aacb8dbf7709f668b76f543729f2a462495afe6
ipramodptdr <ipramodptdr@gmail.com>
2025-03-28 13:04:32 +0530

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import reactRouterDom from 'react-router-dom';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  
+  build: {
+    rollupOptions: {
+      external: [reactRouterDom],
+    },
+  },
 })
